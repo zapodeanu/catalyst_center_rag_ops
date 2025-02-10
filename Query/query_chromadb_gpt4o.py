2e6a708a5aff27b089505889317df83fecf9b753
zapodeanu <gzapodea@cisco.com>
2025-02-10 11:39:20 +0100

    updated libraries used, chain.invoke

--- Query/query_chromadb_gpt4o.py
+++ Query/query_chromadb_gpt4o.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2024 Cisco and/or its affiliates.
+Copyright (c) 2025 Cisco and/or its affiliates.
 This software is licensed to you under the terms of the Cisco Sample
 Code License, Version 1.1 (the "License"). You may obtain a copy of the
 License at
@@ -17,7 +17,7 @@ or implied.
 __author__ = "Gabriel Zapodeanu TME, ENB"
 __email__ = "gzapodea@cisco.com"
 __version__ = "0.1.0"
-__copyright__ = "Copyright (c) 2024 Cisco and/or its affiliates."
+__copyright__ = "Copyright (c) 2025 Cisco and/or its affiliates."
 __license__ = "Cisco Sample Code License, Version 1.1"
 
 import os
@@ -28,18 +28,19 @@ from dotenv import load_dotenv
 # noinspection PyProtectedMember
 from langchain._api import LangChainDeprecationWarning
 from langchain.chains.question_answering import load_qa_chain
-from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_community.vectorstores.chroma import Chroma
 from langchain_openai import ChatOpenAI
+from langchain_huggingface import HuggingFaceEmbeddings
 
 warnings.simplefilter("ignore", category=LangChainDeprecationWarning)  # to disable LangChain warnings
 
-# import the OpenAI Key
+os.chdir('../')
+
 load_dotenv('environment.env')
 
 # database server details
 DB_SERVER = os.getenv('DB_SERVER')
-DB_PORT = os.getenv('DB_PORT')
+DB_PORT = int(os.getenv('DB_PORT'))
 DB_COLLECTION = os.getenv('DB_COLLECTION')
 
 # OpenAI key
@@ -55,16 +56,16 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 def main():
     """
     The app will run a LangChain that will identify similarity results with a query.
-    The match will be provided to the user after running through the GPT-3.5 OpenAI model.
+    The match will be provided to the user after running through the {gpt-4o} OpenAI model.
     """
 
     # Chroma DB server details
     chroma_db_server = chromadb.HttpClient(host=DB_SERVER, port=DB_PORT)
 
     # define the model for creating embeddings for the query
-    embeddings = SentenceTransformerEmbeddings(model_name=MODEL_NAME)
+    embeddings = HuggingFaceEmbeddings(model_name=MODEL_NAME)
 
-    # initialize the Chroma DB connection to server and collection
+    # define the Chroma DB connection to server and collection
     chroma_db = Chroma(
         client=chroma_db_server,
         collection_name=DB_COLLECTION,
@@ -78,14 +79,17 @@ def main():
     query = "y"
     while query != ' ':
         # prompt the user for the query input
-        query = input('\n\nHow may I help you?  ')
+        query = input('\n\nI am a NetOps bot. How may I help you?  ')
 
-        # search for the similarity matches from the Chroma DB server/collection
-        matching_docs = chroma_db.similarity_search(query)
+        query = query.strip()  # remove spaces
+        if query != '':
+            # search for the similarity matches from the Chroma DB server/collection
+            matching_docs = chroma_db.similarity_search(query, k=6)
 
-        # The similarity search result and query will be used to generate the answer using the LLM
-        answer = chain.run(input_documents=matching_docs, question=query, set_verbose=False)
-        print(answer)
+            # The similarity search result and query will be used to generate the answer using the LLM
+            answer = chain.invoke({'input_documents': matching_docs, 'question': query})
+            print(answer['output_text'])
+
     return
 
 
