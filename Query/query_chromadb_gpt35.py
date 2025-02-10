3526255e8daa05ab7800e0f9ef2c532c32ec6ea3
zapodeanu <gzapodea@cisco.com>
2025-02-10 11:41:00 +0100

    updated libraries

--- Query/query_chromadb_gpt35.py
+++ Query/query_chromadb_gpt35.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2024 Cisco and/or its affiliates.
+Copyright (c) 2025 Cisco and/or its affiliates.
 This software is licensed to you under the terms of the Cisco Sample
 Code License, Version 1.1 (the "License"). You may obtain a copy of the
 License at
@@ -14,10 +14,10 @@ IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 or implied.
 """
 
-__author__ = "Gabriel Zapodeanu TME, ENB"
+__author__ = "Gabriel Zapodeanu PTME"
 __email__ = "gzapodea@cisco.com"
 __version__ = "0.1.0"
-__copyright__ = "Copyright (c) 2024 Cisco and/or its affiliates."
+__copyright__ = "Copyright (c) 2025 Cisco and/or its affiliates."
 __license__ = "Cisco Sample Code License, Version 1.1"
 
 import os
@@ -28,18 +28,19 @@ from dotenv import load_dotenv
 # noinspection PyProtectedMember
 from langchain._api import LangChainDeprecationWarning
 from langchain.chains.question_answering import load_qa_chain
-from langchain_community.embeddings import SentenceTransformerEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores.chroma import Chroma
 from langchain_openai import ChatOpenAI
 
 warnings.simplefilter("ignore", category=LangChainDeprecationWarning)  # to disable LangChain warnings
 
-# import the OpenAI Key
+os.chdir('../')
+
 load_dotenv('environment.env')
 
 # database server details
 DB_SERVER = os.getenv('DB_SERVER')
-DB_PORT = os.getenv('DB_PORT')
+DB_PORT = int(os.getenv('DB_PORT'))
 DB_COLLECTION = os.getenv('DB_COLLECTION')
 
 # OpenAi key
@@ -62,7 +63,7 @@ def main():
     chroma_db_server = chromadb.HttpClient(host=DB_SERVER, port=DB_PORT)
 
     # define the model for creating embeddings for the query
-    embeddings = SentenceTransformerEmbeddings(model_name=MODEL_NAME)
+    embeddings = HuggingFaceEmbeddings(model_name=MODEL_NAME)
 
     # initialize the Chroma DB connection to server and collection
     chroma_db = Chroma(
@@ -78,7 +79,7 @@ def main():
     query = "y"
     while query != ' ':
         # prompt the user for the query input
-        query = input('\n\nHow may I help you?  ')
+        query = input('\n\n\n\nI am a NetOps bot. How may I help you?  ')
 
         # search for the similarity matches from the Chroma DB server/collection
         matching_docs = chroma_db.similarity_search(query)
