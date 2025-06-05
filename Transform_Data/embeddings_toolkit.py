b9fedf3a8e986a06c09600f80969ac9e5f31bb9e
zapodeanu <gzapodea@cisco.com>
2025-06-05 11:17:51 -0700

    updated sentence

--- Transform_Data/embeddings_toolkit.py
+++ Transform_Data/embeddings_toolkit.py
@@ -1,12 +1,19 @@
 import logging
-from sentence_transformers import SentenceTransformer
 
+import numpy as np
+from sentence_transformers import SentenceTransformer
 
 # logging, Warning level, to file
 logging.basicConfig(level=logging.DEBUG)
 
-sentences = ["interface Loopback0\n ip address 10.93.141.20 255.255.255.255"]
+sentences = ["The device: PDX-RO details\n Hostname: PDX-RO\n Location: Global/OR/PDX/Floor-2\n Device Role: BORDER ROUTER"]
+
+print('\n\n' +sentences[0] + '\n\n')
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-embeddings = model.encode(sentences)
+embeddings = model.encode(sentences)[0]
+
 print(embeddings)
+
+# Save to a text file
+np.savetxt("output.txt", embeddings)
