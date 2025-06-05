ac260702545acdef1450eea2f8787bde214f295d
zapodeanu <gzapodea@cisco.com>
2025-06-05 11:19:11 -0700

    updated comments, dataset folder

--- Transform_Data/embeddings_to_chroma.py
+++ Transform_Data/embeddings_to_chroma.py
@@ -35,26 +35,28 @@ from langchain_huggingface import HuggingFaceEmbeddings
 os.environ['TZ'] = 'America/Los_Angeles'  # define the timezone for PST
 time.tzset()  # adjust the timezone, more info https://help.pythonanywhere.com/pages/SettingTheTimezone/
 
-# logging, Warning level, to file
-logging.basicConfig(level=logging.WARNING)
+# logging, info level
+logging.basicConfig(level=logging.INFO)
+# create loggers as warning for chromadb, sentence transformers
+logging.getLogger('httpx').setLevel(logging.WARNING)
+logging.getLogger('sentence_transformers').setLevel(logging.WARNING)
+logging.getLogger('chromadb.telemetry').setLevel(logging.WARNING)
 
 load_dotenv('environment.env')
-
-MODEL_NAME = 'all-MiniLM-L6-v2'
 
 # database server details
 DB_SERVER = os.getenv('DB_SERVER')
 DB_PORT = int(os.getenv('DB_PORT'))
 DB_COLLECTION = os.getenv('DB_COLLECTION')
-DB_PATH = os.getenv('DB_PATH')
-
-OPS_DATA = 'OPS_DATA'
+APPS_PATH = os.getenv('APPS_PATH')
+DATASET = os.getenv('DATASET')
+MODEL_NAME = os.getenv('MODEL_NAME')
 
 
 def load_docs(directory):
     """
     This function will load the docs from the specified folder
-    :param directory: folder with the data to be embedded
+    :param directory: the data to be embedded
     :return: documents from folder
     """
     loader = DirectoryLoader(directory)
@@ -62,18 +64,38 @@ def load_docs(directory):
     return documents
 
 
-def split_docs(document, chunk_size, chunk_overlap, separator):
+def split_docs(document, chunk_size, chunk_overlap, separator, file):
     """
-    This function will split the documents with the defined number of characters, overlap, and separator
+    This function will split the documents with the defined number of characters, overlap,
+    and separator. It will add metadata to each chunk. The metadata will be created based
+    on the filename.
     :param document: document to be split
     :param chunk_size: chuck size
     :param chunk_overlap: overlap
     :param separator: separator
+    :param file: filename for the content
     :return: doc split in chunks
     """
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap, separators=separator)
-    docs = text_splitter.split_documents(document)
-    return docs
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,
+                                                   chunk_overlap=chunk_overlap,
+                                                   separators=separator)
+    split_documents = text_splitter.split_documents(document)
+
+    # collect the data for device, issue, command, to be used in metadata
+    file_details = file.split('_')
+
+    device_name = file_details[0]
+    command = file_details[1].replace('-', ' ')
+
+    chunk_number = 1
+    for doc in split_documents:
+        doc.metadata['chunk_number'] = chunk_number  # Add a chunk number as metadata
+        doc.metadata['device name'] = device_name
+        doc.metadata['CLI command'] = command
+        chunk_number += 1
+
+    return split_documents
 
 
 def load_file(filename, path):
@@ -88,12 +110,14 @@ def load_file(filename, path):
     return file_content
 
 
-def create_doc_embeddings(document):
+# noinspection PyProtectedMember,PyUnusedLocal
+def create_doc_embeddings(document, file):
     """
     The function will create the embeddings for the {doc}, with the metadata provided, using
-    {sentence-transformers/all-MiniLM-L6-v2} model.
+    {sentence-transformers/MODEL_NAME} model.
     Update the ChromaDB vector database with the new embeddings
     :param document: document to be embedded
+    :param file: filename for the document
     :return: collection count, after updating it
     """
 
@@ -101,7 +125,9 @@ def create_doc_embeddings(document):
     chroma_db_server = chromadb.HttpClient(host=DB_SERVER, port=DB_PORT)
 
     # split the document, create embeddings
-    docs = split_docs(document=document, chunk_size=100, chunk_overlap=25, separator="!")
+    docs = split_docs(document=document, chunk_size=100, chunk_overlap=25, separator='!', file=file)
+
+    # define embeddings model
     embeddings = HuggingFaceEmbeddings(model_name=MODEL_NAME)
 
     # update the chroma db collection with the new embeddings
@@ -122,15 +148,17 @@ def create_doc_embeddings(document):
 
 def main():
     """
-    This application will load the files from the {OPS_DATA} folder.
-    Each file will be split in chunks, and embeddings will be created for each chunk.
-    The embeddings and chunks will be uploaded to the Chroma DB server.
+    This application will load the files from the {DATASET} folder.
+    Each file will be split in chunks, metadata will be created for each chunk, and
+    embeddings will be created for each chunk.
+    The embeddings will be uploaded to the Chroma DB server.
     """
 
-    logging.warning(' The folder with the data to be embedded is: ' + OPS_DATA)
+    logging.info(' The folder with the data to be embedded is: ' + DATASET)
 
-    documents = load_docs(OPS_DATA)
-    logging.warning(' There are ' + str(len(documents)) + ' documents in the folder')
+    os.chdir(APPS_PATH)
+    documents = load_docs(DATASET)
+    logging.info(' There are ' + str(len(documents)) + ' documents in the folder')
 
     # create the chroma client, and create or get the collection
     chroma_db = chromadb.HttpClient(host=DB_SERVER, port=DB_PORT)
@@ -139,16 +167,16 @@ def main():
     chroma_db.heartbeat()
 
     # load the files from the folder
-    files_list = os.listdir(OPS_DATA)
-    logging.warning(' The files to be embedded are: ')
+    files_list = os.listdir(DATASET)
+    logging.info(' We will create vector representations for these files: ')
 
     # for each file create and update the embeddings
     for file in files_list:
         logging.warning('    ' + file)
-        file_content = load_file(file, OPS_DATA)
-        filename = [file.split(".")[0]]
-        collection_count = create_doc_embeddings(document=file_content)
-        logging.warning(' Collection count is ' + str(collection_count))
+        file_content = load_file(file, DATASET)
+        filename = file.split(".")[0]
+        collection_count = create_doc_embeddings(document=file_content, file=filename)
+        logging.info(' Collection count is ' + str(collection_count))
 
     # chromadb heartbeat
     chroma_db.heartbeat()
